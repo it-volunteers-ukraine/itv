710722cc661f368ec6d0b1ca6acab73ee69e4eff
Artem <krylovartem1994@gmail.com>
2023-10-03 18:42:25 +0300

    feat: added join-the-team template part php and scss

--- templates/home.php
+++ templates/home.php
@@ -5,6 +5,7 @@ Template Name: home
 get_header();
 ?>
 <h2>home page</h2>
+<?php get_template_part('template-parts/join-the-team'); ?>
 
 
 
---
+++ templates/template-parts/join-the-team.php
@@ -0,0 +1,9 @@
+<section class="join-the-team">
+    <h2  class="join-the-team__header">Хочеш приєднатись до команди ІТ волонтерів?</h2>
+    <p  class="join-the-team__text">Хочеш стати частиною нашої команди та бути учасником позитивних змін? Залишай свою заявку і разом ми будемо робити добро через технології.</p>
+    <button type="button" class="join-the-team__button" alt="become IT volunteer">Стати ІТ волотнером</button>
+</section>
+
+
+
+
